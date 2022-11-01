7e54733555cb582fd141341c9c04f23a94f1e277
LucasBoe <lucas@lumotex.de>
2022-11-01 16:10:05 +0100

    Made packages into subrepo and moved content into packages folder

--- Assets/DialogueSystem/Runtime/Interfaces/ILocalizeableTextContainer.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ILocalizeableTextContainer
-{
-    string ContainerName { get; }
-    ILocalizeableText[] GetAllChilds();
-}
